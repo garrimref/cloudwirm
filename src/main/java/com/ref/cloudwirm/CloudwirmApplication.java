18294708e831da9c0f377542db52d820bd99e11a
garrimref <98973113+garrimref@users.noreply.github.com>
2024-09-27 21:00:11 +0200

    add docker compose and base properties

---
+++ src/main/java/com/ref/cloudwirm/CloudwirmApplication.java
@@ -0,0 +1,13 @@
+package com.ref.cloudwirm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudwirmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudwirmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudwirm/cloudwirm/CloudwirmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudwirm.cloudwirm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudwirmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
