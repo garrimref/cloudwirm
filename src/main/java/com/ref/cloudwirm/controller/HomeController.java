a2b9ce14756a9c0eb22820e53729fb52a187d9f6
garrimref <98973113+garrimref@users.noreply.github.com>
2024-10-17 02:50:36 +0200

    update thymeleaf frag - header

--- src/main/java/com/ref/cloudwirm/controller/HomeController.java
+++ src/main/java/com/ref/cloudwirm/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.ref.cloudwirm.controller;
 
 import com.ref.cloudwirm.domain.User;
+import com.ref.cloudwirm.repos.UserRepository;
 import com.ref.cloudwirm.service.FileStorageService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ref.cloudwirm.service.FolderStorageService;
+import com.ref.cloudwirm.service.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +14,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+    private final FileStorageService fileStorageService;
+
+    private final FolderStorageService folderStorageService;
+
+    private final UserRepository userRepository;
+    public HomeController(FileStorageService fileStorageService, FolderStorageService folderStorageService, UserRepository userRepository) {
+        this.fileStorageService = fileStorageService;
+        this.folderStorageService = folderStorageService;
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/")
-    public String index(@AuthenticationPrincipal User user,
+    public String index(@AuthenticationPrincipal UserDetails userDetails,
                         @RequestParam(value = "path", required = false) String path,
                         Model model) {
-        model.addAttribute("user", user);
+        User user = userRepository.findByUsername(userDetails.getUsername());
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("path", path);
+        model.addAttribute("files", fileStorageService.getObjectsList(user.getId(), path, false));
 
-        model.addAttribute("files");
         return "index";
     }
 }
