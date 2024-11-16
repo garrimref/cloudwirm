80e734746333adb188f4c90fb6e7ad6443184cd4
garrimref <98973113+garrimref@users.noreply.github.com>
2024-11-16 21:59:43 +0100

    added name field in Metadata

--- src/main/java/com/ref/cloudwirm/dto/S3ObjectMetaData.java
+++ src/main/java/com/ref/cloudwirm/dto/S3ObjectMetaData.java
@@ -1,22 +1,31 @@
 package com.ref.cloudwirm.dto;
 
 public class S3ObjectMetaData {
-    private String fileName;
+    private String path;
+    private String name;
     private boolean isDir;
     private String folderPrefix;
 
-    public S3ObjectMetaData(String fileName, boolean isDir, String folder) {
-        this.fileName = fileName;
+    public S3ObjectMetaData(String path, String name, boolean isDir, String folder) {
+        this.path = path;
+        this.name = name;
         this.isDir = isDir;
         this.folderPrefix = folder;
     }
 
-    public String getFileName() {
-        return fileName;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
     }
 
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
     }
 
     public boolean isDir() {
